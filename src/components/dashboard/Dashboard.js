63166d0937d7896522975b61fa8013f089f03fc1
fabricien-web <rakotoarimananafabricien@gmail.com>
2019-07-28 15:38:20 +0300

    one love

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import PropAtelier from "../propAtelier/propAtelier";
+import Ajoutatelier from "../newAtelier/NewAtelier"
+
+import Menu from "../design/menu/menu"
+
+ import { Link} from 'react-router-dom'
+class Dashboard extends Component {
+
+  
+onLogoutClick = e => {
+  e.preventDefault();
+  this.props.logoutUser();
+};
+  render() {
+    console.log('localStorage.local sur dashbord'+localStorage.id);
+    
+    const { user } = this.props.auth;
+
+    return (
+      
+      <div>
+       <Menu/>
+      <div className="container-fluid" id="dash">
+      <div className=" valign-wrapper">
+        <div className="container-fluid">
+        
+     
+        <div className="row">
+          <div className="col-md-2">
+            <h4>
+             zah iany
+            </h4>
+            
+            <button className="btn btn-large waves-effect waves-light hoverable  accent-3 bg bg-info" id="ajoutbouton" onClick={()=>{
+              document.getElementById('ajoutatelier').style.display = "block"
+              document.getElementById('tableau').style.display ="none"
+            }}>
+                Ajouter 
+            </button>
+            <button  className="btn btn-large waves-effect waves-light hoverable  accent-3 bg bg-info" id="listerbout" onClick={()=>{
+                  document.getElementById('tableau').style.display = "block"
+                  document.getElementById('ajoutatelier').style.display = "none"
+              }}>
+                  Les ateliers
+              </button>
+            <button  id="deconnect"
+              style={{
+                width: "150px",
+                borderRadius: "5px",
+                
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable  accent-3 bg bg-warning"
+            >
+              Deconnecter
+            </button>
+          </div>
+
+          <div className= "col-md-10">
+          <Ajoutatelier/>
+          <PropAtelier/>
+          </div>
+        </div>
+        </div>
+      </div>
+      
+      </div>
+
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Dashboard);
